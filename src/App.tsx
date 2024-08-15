6e9ffb3bed1cecd251ae291b69789a08c91feb89
gvarma28 <gouravswork@gmail.com>
2024-08-16 01:58:56 +0530

    feat: copy button | footer | github report issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import "./App.css";
 import { Textarea } from "@/components/ui/textarea";
-// import { AxiosRequestConfig } from "axios";
+import GithubIcon from "./assets/github.svg";
 import { axiosToCurl } from "./utils/axios-curl";
 import { Button } from "@/components/ui/button";
 
 function App() {
   const [configInput, setConfigInput] = useState("");
   const [outputCurl, setOutputCurl] = useState("");
+  const [copyState, setCopyState] = useState(false);
 
-  // const testconfig: AxiosRequestConfig = {
-  //   method: "get",
-  //   maxContentLength: Infinity,
-  //   url: "https://www.mymoto.com.au/cars?sort=newest",
-  //   headers: {
-  //     "accept-encoding": "gzip, deflate",
-  //   },
-  //   // data: {"test": "abc"}
-  // };
+  const INPUT_PLACEHOLDER = `Enter your axios config object
+Example:
+{
+  method: "POST",
+  maxContentLength: Infinity,
+  url: "https://www.abcd.com",
+  headers: {
+    "accept-encoding": "gzip, deflate",
+  },
+  data: {"test": "abc"}
+}
+  `;
 
   const handleClick = () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -45,39 +49,73 @@ function App() {
     }
   };
 
+  const handleCopy = async () => {
+    try {
+      navigator.clipboard.writeText(outputCurl);
+      setCopyState(true);
+      setTimeout(() => {
+        setCopyState(false);
+      }, 1500);
+    } catch (error) {
+      console.log("Error while copying to clipboard", error);
+    }
+  };
+
   return (
-    <>
-      <h3 className="flex mt-10 px-2 lg:px-24 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        axios to curl 
-      </h3>
-      {/* <main className="flex min-h-screen flex-col items-center justify-between px-24 py-12 max-p-[20px]"> */}
-      {/* <main className="flex min-h-screen flex-col items-center px-24 py-12 sm:px-0 md:px-0"> */}
-      <main className="flex min-h-screen flex-col items-center px-2 py-7 lg:px-24 lg:py-12">
-        <div className="w-full">
-          <Textarea
-            className="h-96 w-full"
-            value={configInput.toString()}
-            onChange={(e) => {
-              setConfigInput(e.target.value);
-            }}
-            placeholder="Enter your axios config object"
-          />
+    <main className="h-screen p-0 flex flex-col justify-between">
+      <div>
+        <div className="h-4"></div>
+        <div className="flex justify-between mt-7 px-2 lg:px-24 border-b pb-2 tracking-tight transition-colors first:mt-0">
+          <h3 className="text-3xl font-semibold ">Axios To CuRL</h3>
         </div>
-        <div className="w-full m-10">
-          <Textarea
-            className="h-20 resize-none"
-            value={outputCurl}
-            onChange={(e) => {
-              setOutputCurl(e.target.value);
-            }}
-            placeholder="Curl Output"
-          />
+        <div className="flex flex-col items-center px-2 py-7">
+          <div className="w-full">
+            <Textarea
+              className="h-[45vh] w-full"
+              value={configInput.toString()}
+              onChange={(e) => {
+                setConfigInput(e.target.value);
+              }}
+              placeholder={INPUT_PLACEHOLDER}
+            />
+          </div>
+          <div className="w-full m-7 flex justify-between items-center">
+            <Textarea
+              className="h-[7vh] max-h-16 resize-none"
+              value={outputCurl}
+              onChange={(e) => {
+                setOutputCurl(e.target.value);
+              }}
+              placeholder="Output"
+            />
+            <Button
+              className="hidden sm:block ml-3 h-[7vh] max-h-16"
+              variant="outline"
+              onClick={handleCopy}
+            >
+              {!copyState ? "Copy" : "Copied"}
+            </Button>
+          </div>
+          <Button variant="outline" onClick={handleClick} className="m-0">
+            Convert
+          </Button>
         </div>
-        <Button variant="outline" onClick={handleClick}>
-          Let's Go
-        </Button>
-      </main>
-    </>
+      </div>
+      <div className="m-0 py-2 flex justify-between px-2 lg:px-24 border-t pb-2 tracking-tight transition-colors">
+        <div className="flex items-center">
+          <p className="text-sm mx-2">© 2024</p>
+        </div>
+        <div className="flex items-center">
+          <a
+            href="https://github.com/gvarma28/axios-to-curl/issues"
+            className="flex items-center"
+          >
+            <img src={GithubIcon} alt="Github" />
+            <p className="text-sm mx-2">Report a bug</p>
+          </a>
+        </div>
+      </div>
+    </main>
   );
 }
 
